b2c63250eac52fa315377bc81ec8251e18f87795
okapie <tatigreuze@yahoo.co.jp>
2018-07-16 08:17:23 +0900

    Create user detail.
    Code refactoring.

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -1,5 +1,69 @@
 <template>
   <div>
-    <h1>User Detail page.</h1>
+    <h1>User Detail.</h1>
+    <dl>
+      <dt>
+        ID
+      </dt>
+      <dd>
+        {{ member.id }}
+      </dd>
+      <dt>
+        Name
+      </dt>
+      <dd>
+        {{ member.name }}
+      </dd>
+      <dt>
+        Email
+      </dt>
+      <dd>
+        {{ member.email }}
+      </dd>
+    </dl>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    member: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style>
+  h1 {
+    width: 100%;
+  }
+
+  dl {
+    display: flex;
+    flex-wrap: wrap;
+    width: 92%;
+    border: 1px solid #000;
+    margin: 0 4%;
+  }
+
+  dt {
+    width: calc(25% - 9px);
+    border-right: 1px solid #000;
+    border-bottom: 1px solid #000;
+    background: #b0b0b0;
+    padding: 4px;
+  }
+
+  dd {
+    width: calc(75% - 9px);
+    margin: 0;
+    border-bottom: 1px solid #000;
+    padding: 4px;
+  }
+
+  dd:last-of-type,
+  dt:last-of-type {
+    border-bottom: none;
+  }
+</style>
--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,62 +1,100 @@
 <template>
   <div>
-    <h1>Users page.</h1>
-    <dl>
-      <dt></dt>
-      <dd></dd>
-    </dl>
-    <ul class='list-header'>
-      <li>ID</li>
-      <li>Name</li>
-      <li>Email</li>
-    </ul>
-    <ul v-for='member in members' class='list-body'>
-      <li>{{member.id}}
-      <li>{{ member.name }}</li>
-      <li>{{member.email}}</li>
-    </ul>
+    <section>
+      <h1>Users page.</h1>
+      <div class='list-wrapper'>
+        <ul class='list-header'>
+          <li>ID</li>
+          <li>Name</li>
+          <li>Email</li>
+          <li></li>
+        </ul>
+        <ul v-for='(member, index) in members' v-bind:key='member.id' class='list-body'>
+          <li>{{ member.id }}
+          <li>{{ member.name }}</li>
+          <li>{{ member.email }}</li>
+          <li>
+            <button
+              class='detail-button'
+              @click='onChange(index)'
+            >
+              Detail
+            </button>
+          </li>
+        </ul>
+      </div>
+    </section>
+    <section>
+      <app-user-detail
+        :member='member'
+      >
+      </app-user-detail>
+    </section>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
-  export default {
-    data: function () {
-      return {
-        members: []
+import UserDetail from './UserDetail.vue'
+export default {
+  data: function () {
+    return {
+      members: [],
+      member: {
+        id: '3',
+        name: 'takochan',
+        email: 'tako@tako.com'
       }
-    },
-    beforeCreate: function () {
-      axios.get('http://localhost:3000/users.json')
-        .then(res => {
-          console.log(res.data)
-          this.members = res.data
-        })
+    }
+  },
+  beforeCreate: function () {
+    axios.get('http://localhost:3000/users.json')
+      .then(res => {
+        this.members = res.data
+      })
+  },
+  components: {
+    appUserDetail: UserDetail
+  },
+  methods: {
+    onChange (index) {
+      this.member = this.members[index]
     }
   }
+}
 </script>
 
 <style scoped>
+  h1 {
+    width: 100%;
+  }
+
+  .list-wrapper {
+    margin: 0 4%;
+    width: 92%;
+  }
+
   ul {
-    display: flex !important;
+    display: flex;
     flex-wrap: wrap;
     margin: 0;
+    padding: 0;
   }
 
   ul.list-header > li {
-    width: 33%;
+    width: 25%;
     list-style: none;
     background: #b0b0b0;
     box-sizing: border-box;
     border-color: #000;
     border-style: solid;
     border-width: 1px 1px 1px 0;
-    padding: 5px;
+    padding: 4px;
   }
 
   ul.list-body > li {
     text-align: left;
-    width: 33%;
+    width: 25%;
     list-style: none;
     background: #fff;
     box-sizing: border-box;
@@ -80,5 +118,10 @@ import axios from 'axios'
 
   ul.list-body > li:nth-child(2n+1) {
     border-rignt-width: 1px;
+  }
+
+  .detail-button {
+    width: 90%;
+    height: 24px;
   }
 </style>
